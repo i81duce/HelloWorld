6087d1447b3a2af2ca30364adb2f5f6982f57fb7
KevinBozic <bozic.kevin@gmail.com>
2016-02-01 21:48:32 -0500

    Initial Import

---
+++ src/RecBBall.java
@@ -0,0 +1,12 @@
+/**
+ * Created by KevinBozic on 2/1/16.
+ */
+public class RecBBall {
+
+    public String sponsor;
+    public int numPlayers;
+    public String availability; // i.e. Mon-Fri; I'm usually unavailable on Sunday.
+    public String teamColor;
+    public String teamName;
+
+}
