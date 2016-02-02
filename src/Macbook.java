58df8a4618f757c7193813608c2ab5f290d011a5
KevinBozic <bozic.kevin@gmail.com>
2016-02-01 21:35:05 -0500

    Initial Import

---
+++ src/Macbook.java
@@ -0,0 +1,16 @@
+/**
+ * Created by KevinBozic on 2/1/16.
+ */
+public class Macbook {
+
+    public int screenSize;
+    public boolean retina;
+    public String osVersion; /* not sure if this should be float, double, etc... or
+        a String since most current OS's have the following "#.##.#" format
+        */
+    public int ram;
+    public int yearModel;
+    public boolean forceTouch;
+    public String color;
+
+}
